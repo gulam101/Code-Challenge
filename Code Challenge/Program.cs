c9b2d05f3dcdfb487048f470627de3547a14b0c9
unknown <gfram@hotmail.com>
2019-09-22 13:27:33 +0100

    first commit

---
+++ Code Challenge/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Code_Challenge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Please type in a word: ");
+            string userInput = Console.ReadLine();
+            Console.WriteLine($"You have selected {userInput} ");
+
+            Console.Write("Are you sure you want to use this word: ");
+            Request("Change word?");
+
+
+        }
+
+        public static void Request(string userChangedInput)
+        {
+            var confirmWord = false;
+            string changeUserInput = Console.ReadLine();
+
+            if (confirmWord == true)
+            {
+                Console.Write("Please re-type your word in again: ");
+            }
+        }
+
+    }
+}
